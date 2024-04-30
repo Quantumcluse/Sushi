3725243878805193cbd95d3ccef060c851c1984f
Hadi <hadihassan470@gmail.com>
2024-04-30 12:09:25 +0500

    Implimenting operator Overloading in a code

--- Hadi.cpp
+++ Hadi.cpp
@@ -146,7 +146,7 @@ int main() {
     }
     return 0;
 }
-*/
+
 
 
 // implimenting Function Overloading in a code
@@ -185,3 +185,48 @@ cout << endl;
 s1.add(a, b, c);
 }
 
+
+//Operator overloading 
+#include<iostream>
+using namespace std;
+class overload{
+	public:
+		int a;
+		int b;
+	public:
+		overload(int a1, int b1){
+			a=a1;
+			b=b1;
+			int sub=a-b;
+			cout<<"sub:"<<sub<<endl;
+		}
+	void operator-(){
+		a=-a;
+		b=-b;
+	}
+
+    	void operator+(){
+		a=-a;
+		b=-b;
+	}
+
+ //	void operator::(){
+//		-a= +a;
+//		-b= +b;
+//	}
+
+	void display(){
+		cout<<"a:"<<a<<" b:"<<b<<endl;
+	}
+};
+int main(){
+	overload o1(4,3);
+	-o1;
+    	o1.display();
+    +o1;
+    	o1.display();
+   // ::o1;
+	//    o1.display();
+	return 0;
+}
+*/
